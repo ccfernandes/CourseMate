6c8734ec4cb5c218a88bef4ab161034c0d5ca649
Chelsea Fernandes <37026923+ccfernandes@users.noreply.github.com>
2019-08-22 16:27:31 -0700

    working forms

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -6,6 +6,7 @@ from .models import Course
 from .models import SchoolCourse
 from main.forms import CourseForm 
 from main.forms import SchoolCourseForm
+from main.forms import SchoolForm
  
 from django.views.generic import TemplateView
 import datetime
@@ -22,30 +23,73 @@ def review(request):
 	if request.method == "POST":
 		my_form = CourseForm(request.POST)
 		if my_form.is_valid():
-			print(my_form.cleaned_data)
-			Course.objects.create(my_form.cleaned_data)
+			# print(my_form.cleaned_data)
+			# Course.objects.create(my_form.cleaned_data)
 			my_form.save()
+			return render(request=request, template_name="main/course.html", context={'my_form': my_form})
 		else:
 			print(my_form.errors)
+					#once u make a review, it should direct u to the class page 
 	return render(request=request, #so u can reference things inside your template to do with the user
                     template_name="main/review.html", context={"form": my_form})
-					#once u make a review, it should direct u to the class page 
 
-def classPage(request):
-	return render(request=request, #so u can reference things inside your template to do with the user
-                    template_name="main/course.html", context={"form": my_form})
+def SearchPage(request):
+	# def get(self, request):
+	# 	form=CourseForm()
+	# 	posts=Course.objects.all()
+	# 	print(posts)
+	# 	return render(request=request, template_name="main/search.html", context={'form': form})
+	# post=Course.objects.order_by("name")
+	# return render(request=request, #so u can reference things inside your template to do with the user
+ #                    template_name="main/search.html", context={"form": my_form})
+ 	queryset=SchoolCourse.objects.order_by('name')
+ 	return render(request, "main/search.html", context={"object_list": queryset})
+
+def course(request):
+	def get(self, request):
+		form=CourseForm()
+		posts=Course.objects.all()
+		print(posts)
+		return render(request=request, template_name="main/course.html", context={'form': form})
+	post=Course.objects.order_by("name")
+	return render(request=request, template_name="main/course.html", context={})
+
+# def addCourse(request):
+# 	my_form=SchoolForm()
+# 	if request.method=="POST":
+# 		my_form =SchoolForm(request.POST)
+# 		if my_form.is_valid():
+# 			print("Valid")
+# 			my_form.save()
+# 			print("saved)")
+# 			return render(request=request, #so u can reference things inside your template to do with the user
+#                     template_name="main/course.html", context={"form": my_form})
+# 		else:
+# 			my_form=SchoolCourseForm()
+# 			print("error")
+# 			print(my_form.errors)
+# 	print("done")
+# 	return render(request=request, #so u can reference things inside your template to do with the user
+#                     template_name="main/addCourse.html", context={"form": my_form})
 
 def addCourse(request):
+	print("hello")
 	my_form =SchoolCourseForm()
-	if request.method == "POST":
+	if request.method=="POST":
+	# print("post")
 		my_form = SchoolCourseForm(request.POST)
 		if my_form.is_valid():
-			# my_form.save()
-			print(my_form.cleaned_data)
-			SchoolCourse.objects.create(my_form.cleaned_data)
+			my_form.save()
+			print("saved)")
+			return render(request=request, #so u can reference things inside your template to do with the user
+	                  template_name="main/course.html", context={"form": my_form})
+				# print(my_form.cleaned_data)
+				# SchoolCourse.objects.create(my_form.cleaned_data)
 		else:
-			# my_form=SchoolCourseForm()
+			my_form=SchoolCourseForm()
+			print("error")
 			print(my_form.errors)
+	print("done")
 	return render(request=request, #so u can reference things inside your template to do with the user
                     template_name="main/addCourse.html", context={"form": my_form})
 
@@ -57,13 +101,11 @@ def login(request):
 	return render(request=request, template_name="main/login.html", context={'datetime_now':now})
 
 def search(request):
-	return render(request=request, template_name="main/search.html")
+	course_list = SchoolCourse.objects.order_by('name')
+	return render(request=request, template_name="main/search.html", context={"course_list": course_list})
 
 def SCU(request):
 	return render(request=request, template_name="main/SCU.html", context={})
-
-def course(request):
-	return render(request=request, template_name="main/course.html", context={})
 
 def current_date_time(request):
 	now=datetime.datetime.now()
@@ -84,36 +126,46 @@ def current_date_time(request):
 # 		args = {'form': form, 'text': text}
 # 		return render(request=request, template_name="main/review.html", context=args)	
 
-
-# def review_form_submission(request):
-# 	courseNumber=request.POST.get('courseNumber', False)
-# 	name=request.POST.get('name', False)
-
-# 	review_title=request.POST.get('review_title', False)
-# 	review_content=request.POST.get('review_content', False)
-# 	review_published=request.POST.get('review_published', False)
-
-# 	professor_name=request.POST.get('professor_name', False)
-
-# 	gradeReceived=request.POST.get('gradeReceived', False)
-# 	easyRating=request.POST.get('easyRating', False)
-# 	InterestingRating=request.POST.get('InterestingRating', False)
-
-# 	homework=request.POST.get('homework', False)
-# 	pop_quizzes=request.POST.get('pop_quizzes', False)
-# 	webcast=request.POST.get('webcast', False)
-# 	numProjects=request.POST.get('numProjects', False)
-# 	numTests=request.POST.get('numTests', False)
-# 	testDiff=request.POST.get('testDiff', False)
-# 	time_spent=request.POST.get('time_spent', False)
-	
-# 	course_info= Course(courseNumber=courseNumber, name=name, review_title=review_title, review_content=review_content, 
-# 						review_published=review_published,professor_name=professor_name, gradeReceived=gradeReceived, 
-# 						easyRating=easyRating, InterestingRating=InterestingRating,homework=homework, pop_quizzes=pop_quizzes,
-# 						webcast=webcast, numProjects=numProjects, numTests=numTests, testDiff=testDiff, time_spent=time_spent)
+# def addCourse(request):
+# 	my_form=SchoolCourse()
+# 	courseNumber=request.POST.get('courseNumber')
+# 	name=request.POST.get('name')
+# 	course_info = SchoolCourse(courseNumber=courseNumber, name=name)
 # 	course_info.save()
-
 # 	print("Hello form is submitted.")
-# 	return render(request=request, template_name="main/review.html")
+# 	return render(request=request, #so u can reference things inside your template to do with the user
+#                     template_name="main/addCourse.html", context={'my_form': my_form})
+
+
+def review_form_submission(request):
+	courseNumber=request.POST.get('courseNumber', False)
+	name=request.POST.get('name', False)
+
+	review_title=request.POST.get('review_title', False)
+	review_content=request.POST.get('review_content', False)
+	review_published=request.POST.get('review_published', False)
+
+	professor_name=request.POST.get('professor_name', False)
+
+	gradeReceived=request.POST.get('gradeReceived', False)
+	easyRating=request.POST.get('easyRating', False)
+	InterestingRating=request.POST.get('InterestingRating', False)
+
+	homework=request.POST.get('homework', False)
+	pop_quizzes=request.POST.get('pop_quizzes', False)
+	webcast=request.POST.get('webcast', False)
+	numProjects=request.POST.get('numProjects', False)
+	numTests=request.POST.get('numTests', False)
+	testDiff=request.POST.get('testDiff', False)
+	time_spent=request.POST.get('time_spent', False)
+	
+	course_info= Course(courseNumber=courseNumber, name=name, review_title=review_title, review_content=review_content, 
+						review_published=review_published,professor_name=professor_name, gradeReceived=gradeReceived, 
+						easyRating=easyRating, InterestingRating=InterestingRating,homework=homework, pop_quizzes=pop_quizzes,
+						webcast=webcast, numProjects=numProjects, numTests=numTests, testDiff=testDiff, time_spent=time_spent)
+	course_info.save()
+
+	print("Hello form is submitted.")
+	return render(request=request, template_name="main/review.html")
 
  
